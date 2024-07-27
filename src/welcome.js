2f7d2d6d6d470d07e4d16dbc49fd8bd5f6577218
Jahnavichinta <jahnavichinta6@gmail.com>
2024-07-27 10:20:05 +0530

    Added new files

--- src/welcome.js
+++ src/welcome.js
@@ -6,7 +6,7 @@ function Welcome() {
             <Navigation/>
             <div style={{margin:'auto'}}>
                 <center><h2>Welcome</h2></center>
-                <center><h2>Happy Shopping!!!</h2></center>
+                <center><h2>Happy Shopping!!! </h2></center>
             </div>
         </div>
     )
